e3ce75aa57a7a8307bb81895c664a091fe4b0555
deilers1 <dde191@gmail.com>
2012-11-21 19:02:33 -0800

    does some back-end work that helps populate the fact selector boxes

--- CIAWorldFactbook/src/ciaworldfactbook/CIAWorldFactbook.java
+++ CIAWorldFactbook/src/ciaworldfactbook/CIAWorldFactbook.java
@@ -4,11 +4,49 @@
  */
 package ciaworldfactbook;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Devon
  */
-public class CIAWorldFactbook {
-
+public class CIAWorldFactbook 
+{
+    ArrayList<String> countryCommunicationsList;
     
+    public CIAWorldFactbook()
+    {
+        countryCommunicationsList = new ArrayList<String>();
+    }
+    
+    public ArrayList<String> populateCountryCommunicationsList() throws FileNotFoundException
+    {
+        Scanner scan = new Scanner(new File("HeaderDocuments/COUNTRY_COMMUNICATIONS.txt"));
+        
+        
+        while (scan.hasNext())
+        {
+            countryCommunicationsList.add(scan.nextLine().trim());
+        }
+        return countryCommunicationsList;
+    }
+    
+    public String[] getCountryCommunicationList() throws FileNotFoundException
+    {
+        ArrayList<String> temp = new ArrayList<String>();
+        
+        temp = populateCountryCommunicationsList();
+        String[] temp1 = new String[temp.size()];
+        
+        
+        for (int i = 0; i < temp.size(); ++i)
+        {
+            temp1[i] = temp.get(i);
+        }
+        
+        return temp1;
+    }
 }
