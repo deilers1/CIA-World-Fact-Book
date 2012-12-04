f9cde38031353098ba181f15454a1832ec06fae5
deilers1 <dde191@gmail.com>
2012-12-04 15:43:21 -0800

    added an image to the UI

--- CIAWorldFactbook/src/ciaworldfactbook/CIAWordFactbookUI.java
+++ CIAWorldFactbook/src/ciaworldfactbook/CIAWordFactbookUI.java
@@ -60,7 +60,10 @@ public class CIAWordFactbookUI  extends javax.swing.JFrame {
         for (String value : wfb.countryFactTypeList.keySet())
         {
             factTypeSelector.addItem(value);
-        }      
+        }
+        
+        factDisplayTextArea.setEditable(false);
+
     }
 
     /**
@@ -74,7 +77,6 @@ public class CIAWordFactbookUI  extends javax.swing.JFrame {
 
         jMenu1 = new javax.swing.JMenu();
         jComboBox1 = new javax.swing.JComboBox();
-        jLabel1 = new javax.swing.JLabel();
         factTypeSelector = new javax.swing.JComboBox();
         factSelector = new javax.swing.JComboBox();
         getFactButton = new javax.swing.JButton();
@@ -82,11 +84,13 @@ public class CIAWordFactbookUI  extends javax.swing.JFrame {
         exitButton = new javax.swing.JButton();
         factDisplay = new javax.swing.JScrollPane();
         factDisplayTextArea = new javax.swing.JTextArea();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
         jMenu1.setText("jMenu1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(700, 619));
+        setPreferredSize(new java.awt.Dimension(635, 770));
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Select Country" }));
         jComboBox1.addActionListener(new java.awt.event.ActionListener() {
@@ -94,9 +98,6 @@ public class CIAWordFactbookUI  extends javax.swing.JFrame {
                 jComboBox1ActionPerformed(evt);
             }
         });
-
-        jLabel1.setFont(new java.awt.Font("Californian FB", 1, 18)); // NOI18N
-        jLabel1.setText("CIA World Fact Book");
 
         factTypeSelector.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Select Fact Type" }));
         factTypeSelector.addActionListener(new java.awt.event.ActionListener() {
@@ -137,55 +138,63 @@ public class CIAWordFactbookUI  extends javax.swing.JFrame {
         factDisplayTextArea.setRows(5);
         factDisplay.setViewportView(factDisplayTextArea);
 
+        jLabel2.setIcon(new javax.swing.ImageIcon("C:\\Users\\Devon\\Documents\\NetBeansProjects\\CIAWorldFactbook\\src\\image\\cia_factbook.jpg")); // NOI18N
+        jLabel2.setText("jLabel2");
+
+        jLabel3.setText("Image Source: http://www.infotopia.info/images/cia_factbook.jpg");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(83, 83, 83)
+                .addComponent(getFactButton)
+                .addGap(145, 145, 145)
+                .addComponent(clearButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 143, Short.MAX_VALUE)
+                .addComponent(exitButton)
+                .addGap(83, 83, 83))
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(factDisplay)
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 239, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(54, 54, 54)
-                        .addComponent(getFactButton)
-                        .addGap(89, 89, 89)
-                        .addComponent(clearButton)
-                        .addGap(107, 107, 107)
-                        .addComponent(exitButton))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(factDisplay, javax.swing.GroupLayout.PREFERRED_SIZE, 573, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(factSelector, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(factTypeSelector, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 324, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(149, 149, 149))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(factTypeSelector, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(factSelector, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(193, 193, 193))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(2, 2, 2)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel3)
+                .addGap(30, 30, 30)
                 .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(factTypeSelector, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(factSelector, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(factDisplay, javax.swing.GroupLayout.DEFAULT_SIZE, 281, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(factDisplay, javax.swing.GroupLayout.DEFAULT_SIZE, 435, Short.MAX_VALUE)
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(clearButton)
                     .addComponent(getFactButton)
                     .addComponent(exitButton))
-                .addGap(29, 29, 29))
+                .addContainerGap())
         );
 
         pack();
@@ -243,7 +252,6 @@ public class CIAWordFactbookUI  extends javax.swing.JFrame {
         
         factDisplayTextArea.setText("");
         factDisplayTextArea.setLineWrap(true);
-        factDisplayTextArea.setEditable(false);
         factDisplayTextArea.setWrapStyleWord(true);
         if (!countrySelected || !factTypeSelected || !factSelected)
         {
@@ -390,7 +398,8 @@ public class CIAWordFactbookUI  extends javax.swing.JFrame {
     private javax.swing.JComboBox factTypeSelector;
     private javax.swing.JButton getFactButton;
     private javax.swing.JComboBox jComboBox1;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JMenu jMenu1;
     // End of variables declaration//GEN-END:variables
 }
